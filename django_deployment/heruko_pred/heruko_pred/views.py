b341d337f43932de03c47b378ca662dd5831b8e1
UTTEJ KUMAR <72940291+uttej2001@users.noreply.github.com>
2023-10-10 16:56:17 +0530

    added Django deployment

---
+++ django_deployment/heruko_pred/heruko_pred/views.py
@@ -0,0 +1,128 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import pandas as pd
+import numpy as np
+import pickle
+import utils
+import utils2
+def home(request):
+    return render(request, "index.html")
+
+def results(request):
+    year = int(request.POST.get('year', 2000))
+    mileage = float(request.POST.get('mileage'))
+    city = request.POST.get('city')
+    state = request.POST.get('state')
+    make = request.POST.get('make')
+    model = request.POST.get('model')
+    algo = int(request.POST.get('algo'))
+    encoding = int(request.POST.get('encoding'))
+
+    #Convering to a dataframe
+    query = {"Year": [year], "Mileage":[mileage], "City": [city] ,"State":[state], "Make":[make], "Model":[model]}
+    query_df = pd.DataFrame(query)
+    print(query_df)
+	
+
+    with open("models/DT.pkl", "rb") as dt:
+        DT = pickle.load(dt)
+    with open("models/RF.pkl", "rb") as rf:
+        RF = pickle.load(rf)
+    with open("models/Ridge.pkl", "rb") as lr:
+        LR = pickle.load(lr)
+    with open("models/lgbm1.pkl", "rb") as lgbm:
+        LGBM = pickle.load(lgbm)
+    with open("models/data_ver2/lightGBM.pkl", "rb") as lgbm2:
+        LGBM2 = pickle.load(lgbm2)
+#     with open("models/DNN1.h5", "rb") as dnn:
+#         DNN = tf.keras.models.load_model(dnn)
+
+    if encoding ==  1:
+        # Getting Usage Level
+        print(query_df)
+        query_df["Usage_level"] = query_df["Mileage"].apply(utils.get_mile_range)
+        # Getting City Imporane Level
+        print(query_df)
+        query_df["City_imporatnce"] = query_df["City"].apply(utils.city_importance)
+        # Get Mean Price on the city
+        print(query_df)
+        query_df["City_mean_price"] = query_df["City"].apply(utils.get_mean_price)
+        # State Importance
+        print(query_df)
+        query_df["State_imporatnce"] = query_df["State"].apply(utils.state_importance)
+        # Company popularity
+        print(query_df)
+        query_df["Brand_popularity"] = query_df["Make"].apply(utils.popullariy)
+        # Model Level
+        print(query_df)
+        query_df["Model_level"] = query_df["Model"].apply(utils.model_level)
+        #Bofore One Hot encoding
+        print(query_df["Make"])
+        
+        
+        
+        query_df = query_df.drop(columns = ["Make", "Model", "City"])
+        print(query_df)
+        # Choose Encoding encoding
+        query_df = utils.oh_encoder(query_df)
+
+        # Decide the model
+        if algo == 1:
+            model_predict = LR.predict(query_df)
+            r2_score = 0.608
+            nrmse_score = 0.1
+            mape = 0.32
+
+        elif algo == 2:
+            model_predict = DT.predict(query_df)
+            r2_score = 0.67
+            nrmse_score = 0.09
+            mape = 0.24
+
+        elif algo == 3:
+            model_predict = RF.predict(query_df)
+            r2_score = 0.68
+            nrmse_score = 0.09
+            mape = 0.24
+        
+
+        # elif algo == 5:
+        #     model_predict = DNN.predict(query_df)
+        #     r2_score = 0.39
+        #     nrmse_score = "Not Calculated"
+        #     mape = "Not Calculated"
+
+        else:
+            model_predict = LGBM.predict(query_df)
+            r2_score = 0.85
+            nrmse_score = "Not Calculated"
+            mape = "Not Calculated"
+
+
+
+    else: 
+        #Getting ready for label encoding
+        query_df["Usage_level"] = query_df["Mileage"].apply(utils.get_mile_range)
+        query_df["City_imporatnce"] = query_df["City"].apply(utils.city_importance)
+        query_df["Company_popularity"] = query_df["Make"].apply(utils2.Company_Popularity)
+        query_df["Luxury"] = query_df["Model"].apply(utils2.luxury)
+        #Label Encoder
+        query_df["City_n"] = utils2.city_encoder(query_df["City"])
+        query_df["State_n"] = utils2.state_encoder(query_df["State"])
+        query_df["Make_n"] = utils2.brand_encoder(query_df["Make"])
+        query_df["Model_n"] = utils2.model_encoder(query_df["Model"])
+        query_df=query_df.drop(["City","State","Make","Model"],axis="columns")
+        query_df = utils2.convert_into_cat(query_df)            
+
+        #One Hot Encoder encoding
+        query_enc = utils2.one_hot_encoding(query_df)
+        query_df = pd.concat([query_df, query_enc], axis=1)
+        query_df=query_df.drop(["City_imporatnce","Usage_level","Company_popularity","Luxury"],axis="columns")
+
+        model_predict = LGBM2.predict(query_df)
+        r2_score = 0.85
+        nrmse_score = 0.09
+        mape = 0.24
+
+    
+    return render(request, "results.html", {"predictions":model_predict, "score":  r2_score, "nrmse": nrmse_score})
